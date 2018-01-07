ae764dc42f2e025690519f7f1a01ab76aa451cc6
First Last <flatironcamp.tc99@mamber.net>
2018-01-07 05:44:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  return (Object.assign(obj, obj, {[key]: value}));
+}
+
+function deleteFromObjectByKey(obj, key) {
+  
+  // returns "true" even if key doesn not exist in object, for simple stuff
+  delete obj[key];
+  
+  return Object.assign({}, obj);
+}
