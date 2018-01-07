8236a97b3e0c69680e9af9296783d2e1d7739fd4
First Last <flatironcamp.tc99@mamber.net>
2018-01-07 05:45:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  return (Object.assign(obj, obj, {[key]: value}));
+ //  return (Object.assign(obj, obj, {[key]: value}));
+ return obj[key]=value;
 }
 
 function deleteFromObjectByKey(obj, key) {
